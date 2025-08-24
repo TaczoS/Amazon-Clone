be696316aadc052022a63062cf393ff80a300d48
TaczoS <shayansiddiqui20069@gmail.com>
2025-08-24 19:46:47 +0530

    used query selector to remove the element from html

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -14,7 +14,7 @@ cart.forEach((cartItem) => {
  
   cartSummaryHTML += 
   `
-  <div class="cart-item-container">
+  <div class="cart-item-container js-cart-item-container-${productId}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -96,5 +96,7 @@ document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
 document.querySelectorAll('.js-delete-link').forEach((link) => {link.addEventListener('click',()=> {
   const productId = link.dataset.productId;
   removeFromCart(productId);
-  console.log(cart);
+  const container = document.querySelector(`.js-cart-item-container-${productId}`);
+  container.remove();
+  
 });});
